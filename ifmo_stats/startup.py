95927dc8de87526a250f1b2fe5e1767f4dad851f
Nikita Vasilev <dnfletter@gmail.com>
2014-11-25 10:43:32 +0400

    Tabs patch is working

--- ifmo_stats/startup.py
+++ ifmo_stats/startup.py
@@ -1,7 +1,6 @@
 
 from django.conf.urls import include, url
 from path import path
-import django.contrib.staticfiles
 from xmodule.tabs import *
 from xmodule import tabs
 import edxmako
@@ -12,15 +11,22 @@ def patch_templates():
     edxmako.paths.add_lookup('main', template_path, prepend=True)
 
 
-
 def _patch_variant_url(app_name, urls_module):
     app_urls = __import__('%s.urls' % (app_name,), fromlist=['urlpatterns'])
     app_urls.urlpatterns.insert(0, url(r'', include(urls_module)))
-    
+
+
 def patch_tabs():
     tabs.StatsTab = StatsTab
-    tabs.CourseTab.from_json = from_json
-    tabs.CourseTab.iterate_displayable = iterate_displayable
+    tabs.CourseTabList.iterate_displayable = patch_iterate_displayable(tabs.CourseTabList.iterate_displayable)
+
+
+def patch_urls():
+    try:
+        _patch_variant_url('lms', 'ifmo_stats.urls')
+    except Exception:
+        pass
+
 
 class StatsTab(CourseTab):
     type = 'stats'
@@ -33,82 +39,22 @@ class StatsTab(CourseTab):
         )
 
 
-def patch_urls():
-    try:
-        _patch_variant_url('lms', 'ifmo_stats.urls')
-    except Exception:
-        pass
-
-
-
-
-
-def from_json(tab_dict):
-        """
-        Deserializes a CourseTab from a json-like representation.
-        The subclass that is instantiated is determined by the value of the 'type' key in the
-        given dict-type tab. The given dict-type tab is validated before instantiating the CourseTab object.
-        Args:
-            tab: a dictionary with keys for the properties of the tab.
-        Raises:
-            InvalidTabsException if the given tab doesn't have the right keys.
-        """
-        sub_class_types = {
-            'courseware': CoursewareTab,
-            'course_info': CourseInfoTab,
-            'wiki': WikiTab,
-            'discussion': DiscussionTab,
-            'external_discussion': ExternalDiscussionTab,
-            'external_link': ExternalLinkTab,
-            'textbooks': TextbookTabs,
-            'pdf_textbooks': PDFTextbookTabs,
-            'html_textbooks': HtmlTextbookTabs,
-            'progress': ProgressTab,
-            'static_tab': StaticTab,
-            'peer_grading': PeerGradingTab,
-            'staff_grading': StaffGradingTab,
-            'open_ended': OpenEndedGradingTab,
-            'notes': NotesTab,
-            'syllabus': SyllabusTab,
-            'instructor': InstructorTab,  # not persisted
-            'stats': StatsTab,
-        }
-
-        tab_type = tab_dict.get('type')
-        if tab_type not in sub_class_types:
-            raise InvalidTabsException(
-                'Unknown tab type {0}. Known types: {1}'.format(tab_type, sub_class_types)
-            )
-
-        tab_class = sub_class_types[tab_dict['type']]
-        tab_class.validate(tab_dict)
-        return tab_class(tab_dict=tab_dict)
-
-def iterate_displayable(
+def patch_iterate_displayable(func):
+    @staticmethod
+    def patch(
             course,
             settings,
             is_user_authenticated=True,
             is_user_staff=True,
             is_user_enrolled=False
     ):
-        """
-        Generator method for iterating through all tabs that can be displayed for the given course and
-        the given user with the provided access settings.
-        """
-        for tab in course.tabs:
-            if tab.can_display(
-                    course, settings, is_user_authenticated, is_user_staff, is_user_enrolled
-            ) and (not tab.is_hideable or not tab.is_hidden):
-                if tab.is_collection:
-                    for item in tab.items(course):
-                        yield item
-                else:
-                    yield tab
-        instructor_tab = InstructorTab()
-        if instructor_tab.can_display(course, settings, is_user_authenticated, is_user_staff, is_user_enrolled):
-            yield instructor_tab
+        for el in func(course, settings, is_user_authenticated, is_user_staff, is_user_enrolled):
+            yield el
         ifmo_stats = StatsTab()
-        yield ifmo_stats
+        if ifmo_stats.can_display(course, settings, is_user_authenticated, is_user_staff, is_user_enrolled):
+            yield ifmo_stats
+    return patch
+
 
 def run():
     patch_templates()
